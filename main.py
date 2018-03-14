96fc3e78faa43cdf0c9467ed018e8234a1e801ca
Luis Hirvin <uidk4253@zuv1280u.cw01.contiwan.com>
2018-03-13 22:42:54 -0600

    Video Class version 1.0 estable

--- main.py
+++ main.py
@@ -14,26 +14,64 @@ from PyQt5.QtGui import QIcon
 import sys
 
 # Constantes del sistema
-_PATH_VIDEO = "/home/uidk4253/Documents/Hirvin/Projectos/Ted/video.mp4"
+_PATH_VIDEO = "/home/uidk4253/Documents/Hirvin/Projectos/Ted/video2.mp4"
 
- 
+class VideoPlayer(object):
+    """VideoPlayer, defines all methods for video player"""
+    def __init__(self):
+        #video elements
+        self.video_widget = QVideoWidget()
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.media_player.setVideoOutput(self.video_widget)
+        # slider elements
+        self.video_slider = QSlider(Qt.Horizontal)
+        self.video_slider.setRange(0, 0)
+        # layout
+        self.video_layout = QVBoxLayout()
+        self.video_layout.addWidget(self.video_widget)
+        self.video_layout.addWidget(self.video_slider)
+        # signals
+        self.media_player.durationChanged.connect(self.duration_changed)
+        self.media_player.positionChanged.connect(self.position_changed)
+        self.media_player.error.connect(self.video_handle_error)
+        self.video_slider.sliderMoved.connect(self.set_new_video_position)
+
+    def video_handle_error(self):
+        #self.playButton.setEnabled(False)
+        print "Error Video: " + self.media_player.errorString()
+
+    def set_new_video_position(self, position):
+        self.media_player.setPosition(position)
+
+    def position_changed(self, position):
+        #if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+        #    if position > 10000:
+        #        print position
+        #        self.mediaPlayer.pause()
+        self.video_slider.setValue(position)
+
+    def duration_changed(self, duration):
+        self.video_slider.setRange(0, duration)
+
+    def set_video_file(self, video_path):
+        """set de video and start to play it"""
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(video_path)))
+        self.media_player.play()
+
 class VideoWindow(QMainWindow):
  
     def __init__(self, parent=None):
         super(VideoWindow, self).__init__(parent)
         self.setWindowTitle("PyQt Video Player Widget Example - pythonprogramminglanguage.com") 
         
-        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
- 
-        videoWidget = QVideoWidget()
+        #self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        #videoWidget = QVideoWidget()
+        self.video_player = VideoPlayer()
  
         self.playButton = QPushButton()
         self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self.playButton.clicked.connect(self.play_button)
  
-        self.positionSlider = QSlider(Qt.Horizontal)
-        self.positionSlider.setRange(0, 0)
-        self.positionSlider.sliderMoved.connect(self.setPosition)
  
         self.errorLabel = QLabel()
         self.errorLabel.setText("Este es el ejemplo de una etiqueta")
@@ -48,10 +86,9 @@ class VideoWindow(QMainWindow):
         controlLayout = QHBoxLayout()
         controlLayout.setContentsMargins(0, 0, 0, 0)
         controlLayout.addWidget(self.playButton)
-        controlLayout.addWidget(self.positionSlider)
  
         layout = QVBoxLayout()
-        layout.addWidget(videoWidget)
+        layout.addLayout(self.video_player.video_layout)
         layout.addLayout(controlLayout)
         layout.addWidget(self.errorLabel)
  
@@ -59,15 +96,13 @@ class VideoWindow(QMainWindow):
         wid.setLayout(layout)
  
         # conectando la senales
-        self.mediaPlayer.setVideoOutput(videoWidget)
-        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
-        self.mediaPlayer.positionChanged.connect(self.positionChanged)
-        self.mediaPlayer.durationChanged.connect(self.durationChanged)
-        self.mediaPlayer.error.connect(self.handleError)
+        ## Recordar #######################################3
+        #self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        
 
         # incializando el video de forma automatica
-        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile(_PATH_VIDEO)))
-        self.mediaPlayer.play()
+        self.video_player.set_video_file(_PATH_VIDEO)
+        #hasta aqui agregar
  
     # salida de la applicacion
     def exitCall(self):
@@ -86,23 +121,6 @@ class VideoWindow(QMainWindow):
         else:
             self.playButton.setIcon(
                     self.style().standardIcon(QStyle.SP_MediaPlay))
- 
-    def positionChanged(self, position):
-        #if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
-        #    if position > 10000:
-        #        print position
-        #        self.mediaPlayer.pause()
-        self.positionSlider.setValue(position)
- 
-    def durationChanged(self, duration):
-        self.positionSlider.setRange(0, duration)
- 
-    def setPosition(self, position):
-        self.mediaPlayer.setPosition(position)
- 
-    def handleError(self):
-        self.playButton.setEnabled(False)
-        self.errorLabel.setText("Error: " + self.mediaPlayer.errorString())
  
  
 if __name__ == '__main__':
